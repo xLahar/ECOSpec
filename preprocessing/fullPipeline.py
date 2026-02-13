f61f7030817efc171f20b06a9fdae15476c6f815
LanodArisato <123771043+LanodArisato@users.noreply.github.com>
2026-02-13 17:06:08 -0500

    Add TODO comments for command-line argument parsing and processing tasks

--- preprocessing/fullPipeline.py
+++ preprocessing/fullPipeline.py
@@ -1,3 +1,8 @@
+## TODO:
+# - Add command-line argument parsing for input file (see preprocessTest.py)
+# - Process raw test specrra and add to library
+# - test matching with Pearson correlation
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
